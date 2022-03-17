89c12f5b728c6f0e6773bddfde653c171ab089ac
muazakis <muaz.umeyr.akis@gmail.com>
2022-03-17 14:08:19 +0300

    Projenin ilk hali

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 int main(void){
     int dizi[5]={1,2,3,4,5};
 
-    
+    int *parray[10];
+    parray[0]=&dizi[0];
+
+    while(parray[0]<=&dizi[4]){
+        printf("dizinin elemanları: %d\n", *parray[0]);
+
+        parray[0]++;
+
+    }
 
 
     return 0;
